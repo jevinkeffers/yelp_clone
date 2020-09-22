24d87bea62666f487e70c49638080056b5648543
Kevin Jeffers <jevinkeffers@gmail.com>
2020-09-22 01:30:17 -0400

    Many more changes

--- models/yelpModel.js
+++ models/yelpModel.js
@@ -25,13 +25,13 @@ class restaurantList {
         }
     };
 
-    static async postReview(review_title, review, reviewer_score, reviewer_id, restaurant_id) {
+    static async postReview(review_title, review, reviewer_score, restaurant_id) {
         try {
-        await db.any(`INSERT INTO reviews (review_title, review, reviewer_score, reviewer_id, restaurant_id) VALUES ($1, $2, $3, $4, $5);`[review_title, review, reviewer_score, reviewer_id, restaurant_id]);
+        await db.any(`INSERT INTO reviews (review_title, review, reviewer_score, reviewer_id, restaurant_id) VALUES ($1, $2, $3, $4, $5);`[review_title, review, reviewer_score, 5, restaurant_id]);
         } catch (error) {
             return error.message;
         }
-};
+    };
 };
 
 module.exports = restaurantList;
--- routes/businessRoute.js
+++ routes/businessRoute.js
@@ -2,20 +2,25 @@ const express = require("express"),
     router = express.Router(),
     restaurantModel = require ("../models/yelpModel");
 
-    router.get ("/:restaurant?", async function (req, res) {
-        const singleRestaurantData = await restaurantModel.getOne(req.params.restaurant);
-            return res.render("template", {
-                locals: {
-                    title: "Fake Yelp",
-                    data: singleRestaurantData,
-                    is_logged_in: req.session.is_logged_in,
-                },
-                partials: {
-                    partial: "partial-business"
-                },
-            });
-    });
+router.get ("/:restaurant?", async function (req, res) {
+    const singleRestaurantData = await restaurantModel.getOne(req.params.restaurant);
+        return res.render("template", {
+            locals: {
+                title: "Fake Yelp",
+                data: singleRestaurantData,
+                is_logged_in: req.session.is_logged_in,
+            },
+            partials: {
+                partial: "partial-business"
+            },
+        });
+});
 
+router.post("/:restaurant?", async (req, res) => {
+    const{ title, review, reviewer_score, restaurant_id} = req.body;
+    await restaurantModel.addReview(title, review, reviewer_score, restaurant_id);
+    res.redirect("/");
+});
     
 
 module.exports = router;
