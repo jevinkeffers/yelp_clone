57d519131098513812a68ddbefc7d7bc22444964
Kevin Jeffers <jevinkeffers@gmail.com>
2020-10-04 14:51:52 -0400

    Added review functionality thanks to Chris

--- models/yelpModel.js
+++ models/yelpModel.js
@@ -14,7 +14,7 @@ class restaurantList {
     } catch (error) {
         return error.message;
     }
-    }
+    };
     
     static async getOne(slug) {
         try {
@@ -25,13 +25,24 @@ class restaurantList {
         }
     };
 
-    static async postReview(review_title, review, reviewer_score, restaurant_id) {
+    static async addReview(review_title, review, reviewer_score, reviewer_id, restaurant_id) {
         try {
-        await db.any(`INSERT INTO reviews (review_title, review, reviewer_score, reviewer_id, restaurant_id) VALUES ($1, $2, $3, $4, $5);`[review_title, review, reviewer_score, 5, restaurant_id]);
+        const response = await db.result(`INSERT INTO reviews (review_title, review, reviewer_score, reviewer_id, restaurant_id) VALUES ($1, $2, $3, $4, $5);`, [review_title, review, reviewer_score, reviewer_id, restaurant_id]);
+        console.log(response);
         } catch (error) {
             return error.message;
         }
     };
+
+    static async getReviewsByRestaurant(slug) {
+        try {
+            const response = await db.any(`SELECT * FROM reviews INNER JOIN restaurants ON reviews.restaurant_id = restaurants.id INNER JOIN reviewer ON reviews.reviewer_id = reviewer.id WHERE restaurants.slug = '${slug}';`);
+            return response;
+        } catch (error) {
+            return error.message;
+        }
+    };
+
 };
 
 module.exports = restaurantList;
--- routes/businessRoute.js
+++ routes/businessRoute.js
@@ -4,10 +4,12 @@ const express = require("express"),
 
 router.get ("/:restaurant?", async function (req, res) {
     const singleRestaurantData = await restaurantModel.getOne(req.params.restaurant);
+    const reviewData = await restaurantModel.getReviewsByRestaurant(req.params.restaurant)
         return res.render("template", {
             locals: {
                 title: "Fake Yelp",
-                data: singleRestaurantData,
+                singleRestaurantData: singleRestaurantData,
+                reviewData: reviewData,
                 is_logged_in: req.session.is_logged_in,
             },
             partials: {
@@ -17,9 +19,9 @@ router.get ("/:restaurant?", async function (req, res) {
 });
 
 router.post("/:restaurant?", async (req, res) => {
-    const{ title, review, reviewer_score, restaurant_id} = req.body;
-    await restaurantModel.addReview(title, review, reviewer_score, restaurant_id);
-    res.redirect("/");
+    const{ review_title, review, reviewer_score, restaurant_id, restaurant_slug} = req.body;
+    restaurantModel.addReview(review_title, review, reviewer_score, req.session.user_id, restaurant_id);
+    res.redirect(`/business/${restaurant_slug}`);
 });
     
 
